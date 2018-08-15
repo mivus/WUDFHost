c4c8e4ba8d41a2ffcf92cd804971e0a988c9ae0c
mivus <mivuss@gmail.com>
2018-08-15 03:20:14 +0200

    Commit at: 08/15/2018 03:20:14

---
+++ abc/token.ps1
@@ -0,0 +1,9 @@
+﻿$Token = 'b8d55e29c59801a51a2000c487d93c7f0d71c9a3'
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+
+$Headers = @{
+  Authorization = 'Basic {0}' -f $Base64Token;
+  };
+  
+    Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/user -Body '{ "Name": "XXX" }' -Method Patch
+
